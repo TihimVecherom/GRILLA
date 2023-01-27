af23e9b01fe25c5beb3d9774a492f15a3df14124
TihimVecherom <98401044+TihimVecherom@users.noreply.github.com>
2023-01-27 11:43:46 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -322,6 +322,61 @@
             }
         }
     }
+    class MousePRLX {
+        constructor(props, data = null) {
+            let defaultConfig = {
+                init: true,
+                logging: true
+            };
+            this.config = Object.assign(defaultConfig, props);
+            if (this.config.init) {
+                const paralaxMouse = document.querySelectorAll("[data-prlx-mouse]");
+                if (paralaxMouse.length) {
+                    this.paralaxMouseInit(paralaxMouse);
+                    this.setLogging(`Проснулся, слежу за объектами: (${paralaxMouse.length})`);
+                }
+            }
+        }
+        paralaxMouseInit(paralaxMouse) {
+            paralaxMouse.forEach((el => {
+                const paralaxMouseWrapper = el.closest("[data-prlx-mouse-wrapper]");
+                const paramСoefficientX = el.dataset.prlxCx ? +el.dataset.prlxCx : 100;
+                const paramСoefficientY = el.dataset.prlxCy ? +el.dataset.prlxCy : 100;
+                const directionX = el.hasAttribute("data-prlx-dxr") ? -1 : 1;
+                const directionY = el.hasAttribute("data-prlx-dyr") ? -1 : 1;
+                const paramAnimation = el.dataset.prlxA ? +el.dataset.prlxA : 50;
+                let positionX = 0, positionY = 0;
+                let coordXprocent = 0, coordYprocent = 0;
+                setMouseParallaxStyle();
+                if (paralaxMouseWrapper) mouseMoveParalax(paralaxMouseWrapper); else mouseMoveParalax();
+                function setMouseParallaxStyle() {
+                    const distX = coordXprocent - positionX;
+                    const distY = coordYprocent - positionY;
+                    positionX += distX * paramAnimation / 1e3;
+                    positionY += distY * paramAnimation / 1e3;
+                    el.style.cssText = `transform: translate3D(${directionX * positionX / (paramСoefficientX / 10)}%,${directionY * positionY / (paramСoefficientY / 10)}%,0);`;
+                    requestAnimationFrame(setMouseParallaxStyle);
+                }
+                function mouseMoveParalax(wrapper = window) {
+                    wrapper.addEventListener("mousemove", (function(e) {
+                        const offsetTop = el.getBoundingClientRect().top + window.scrollY;
+                        if (offsetTop >= window.scrollY || offsetTop + el.offsetHeight >= window.scrollY) {
+                            const parallaxWidth = window.innerWidth;
+                            const parallaxHeight = window.innerHeight;
+                            const coordX = e.clientX - parallaxWidth / 2;
+                            const coordY = e.clientY - parallaxHeight / 2;
+                            coordXprocent = coordX / parallaxWidth * 20;
+                            coordYprocent = coordY / parallaxHeight * 80;
+                        }
+                    }));
+                }
+            }));
+        }
+        setLogging(message) {
+            this.config.logging ? functions_FLS(`[PRLX Mouse]: ${message}`) : null;
+        }
+    }
+    modules_flsModules.mousePrlx = new MousePRLX({});
     let formValidate = {
         getErrors(form) {
             let error = 0;
@@ -4744,25 +4799,6 @@
     ingridient.forEach((el => {
         el.addEventListener("click", (() => {
             el.classList.toggle("_ingredient-active");
-        }));
-    }));
-    const script_events = document.querySelectorAll(".events__section");
-    script_events.forEach((eve => {
-        let buttonOpens = eve.querySelector(".events__button");
-        let adds = eve.querySelector(".events__block-min");
-        let contineText = eve.querySelector(".contine-text-event");
-        let closeEvent = eve.querySelector(".close-event");
-        buttonOpens.addEventListener("click", (() => {
-            buttonOpens.classList.toggle("_button-events-active");
-            if (contineText) contineText.classList.toggle("_events-text-active");
-            if (closeEvent) closeEvent.classList.toggle("_close-event-active");
-            if (adds) adds.classList.toggle("_events-active");
-        }));
-        closeEvent.addEventListener("click", (() => {
-            closeEvent.classList.toggle("_close-event-active");
-            if (contineText) contineText.classList.toggle("_events-text-active");
-            if (buttonOpens) buttonOpens.classList.toggle("_button-events-active");
-            if (adds) adds.classList.toggle("_events-active");
         }));
     }));
     window["FLS"] = true;
